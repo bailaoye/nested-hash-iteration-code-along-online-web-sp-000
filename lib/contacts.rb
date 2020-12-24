aa27b85a0ac132e369b6333fb7097be0da059de6
Jeff Bai <j.bai@wustl.edu>
2020-12-24 09:14:41 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,8 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
-        value.each do |flavor|
-        end
+        value.delete_if {|flavor| flavor == "strawberry"}
       end
     end
   end
